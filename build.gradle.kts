17f0f1fa7b5be5f6e2a4e6a15cee010a6ef10d54
Nilson Aguiar <5804096+nilson-aguiar@users.noreply.github.com>
2026-04-12 17:38:13 +0200

    chore(build): configure GraalVM native image with picocli-codegen

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.2.21"
+    kotlin("kapt") version "2.2.21"
     id("org.graalvm.buildtools.native") version "0.11.5"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21"
     application
@@ -24,10 +25,11 @@ repositories {
 
 dependencies {
     implementation("info.picocli:picocli:4.7.6")
+    kapt("info.picocli:picocli-codegen:4.7.6")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.microsoft.playwright:playwright:1.44.0")
     implementation("org.jsoup:jsoup:1.18.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
-
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
     testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
@@ -35,6 +37,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
+
 
 kotlin {
     compilerOptions {
@@ -44,4 +47,18 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+graalvmNative {
+    binaries {
+        named("main") {
+            imageName.set("manga-fetcher")
+            mainClass.set("com.mangafetcher.downloader.DownloaderApplicationKt")
+            buildArgs.add("--verbose")
+            buildArgs.add("--no-fallback")
+            buildArgs.add("--initialize-at-build-time=kotlin.DeprecationLevel")
+            // Playwright and OkHttp might need these
+            buildArgs.add("-H:+ReportExceptionStackTraces")
+        }
+    }
 }
